7897fc03b4be08c1ab44ebafded0c08b93a97d56
TheBlutsBrother <zocklein.l@gmail.com>
2024-07-03 13:53:02 -0400

    Feat: idk what I changed.. I forgot

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,6 +7,8 @@ const JUMP_VELOCITY = -250.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -20,6 +22,22 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	#changes sprite horizontally depending on the last direction input
+	if direction > 0:
+		animated_sprite.flip_h = false
+	if direction < 0:
+		animated_sprite.flip_h = true
+
+	#if no direction is detected and is on_floor: play "idel" else play "run"
+	if is_on_floor():
+		if  direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
